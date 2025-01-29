141d5c0b1e00f3c3bdc8d49715a4d7e08e388730
TimLefever <detimster00@gmail.com>
2025-01-29 15:04:22 +0100

    initial push

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+    return (
+        <div className="text-center min-h-screen h-full flex-col justify-center items-center gap-y-20 w-full">
+            <h1 className="text-4xl font-bold">Tim Lefever</h1>
+        </div>
+    );
+}
